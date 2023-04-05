799ada69ff011cb867b09444a147dd22cd36827e
Rob Garner <rgarner011235@gmail.com>
2023-04-04 18:34:20 -0600

    Added functionality to read toppings for the pizza

--- PizzaPointOfSaleSystem/Repos/PizzaRepo.cs
+++ PizzaPointOfSaleSystem/Repos/PizzaRepo.cs
@@ -10,10 +10,12 @@ namespace PizzaPointOfSaleSystem.Repos
 {
     public class PizzaRepo
     {
+        const string CONNECTIONSTRING = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\rgarn\\source\\repos\\CIS-1280-Spring-2023\\PizzaPointOfSaleSystem\\PizzaPointOfSaleSystem\\Data\\PizzaPointOfSaleDB.mdf;Integrated Security=True";
+
         public List<Pizza> GetAll()
         {
             List<Pizza> list = new List<Pizza>();
-            using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\rgarn\\source\\repos\\CIS-1280-Spring-2023\\PizzaPointOfSaleSystem\\PizzaPointOfSaleSystem\\Data\\PizzaPointOfSaleDB.mdf;Integrated Security=True"))
+            using (SqlConnection connection = new SqlConnection(CONNECTIONSTRING))
             {
                 SqlCommand command = new SqlCommand(
                   "SELECT * FROM Pizza;",
@@ -30,6 +32,22 @@ namespace PizzaPointOfSaleSystem.Repos
                         pizza.Id = reader.GetInt32(0);
                         pizza.TypeOfCrust = reader.GetString(1);
                         pizza.Size = reader.GetString(2);
+
+                        //Get toppings and add them to pizza
+                        SqlCommand toppinigsCmd = new SqlCommand("SELECT ToppingId As Id, Name FROM PizzaToppingCrossref LEFT JOIN Topping ON PizzaToppingCrossref.ToppingId = Topping.Id WHERE PizzaId = @Id");
+                        toppinigsCmd.Parameters.AddWithValue("Id", pizza.Id);
+                        pizza.Toppings = new List<Topping>();
+                        SqlDataReader toppingsReader = toppinigsCmd.ExecuteReader();
+                        if (toppingsReader.HasRows)
+                        {
+                            while (toppingsReader.Read())
+                            {
+                                Topping topping = new Topping();
+                                topping.Id = toppingsReader.GetInt32(0);
+                                topping.Name = toppingsReader.GetString(1);
+                                pizza.Toppings.Add(topping);
+                            }                            
+                        }
                         list.Add(pizza);
                     }
                 }
